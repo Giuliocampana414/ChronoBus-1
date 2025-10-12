27ae9086d635c90e809a09e42226e5adadc836ef
Giulio <giulio.campana@buonarroti.tn.it>
2025-10-12 22:13:45 +0200

    fix: update GOOGLE_CLIENT_ID to PUBLIC_GOOGLE_CLIENT_ID

--- src/routes/API/v2/session/loginwithGoogle/+server.js
+++ src/routes/API/v2/session/loginwithGoogle/+server.js
@@ -3,15 +3,15 @@ import jwt from 'jsonwebtoken';
 import { JWT_PASSWORD } from '$env/static/private';
 import { User,mongoose } from '$lib/utils/mongodb.js';
 import { json } from '@sveltejs/kit';
-import { GOOGLE_CLIENT_ID } from '$env/static/private';
+import { PUBLIC_GOOGLE_CLIENT_ID } from '$env/static/public';
 import { ALLOWED_ORIGIN } from '$env/static/private';
 
 // Inizializza il client Google
-const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+const client = new OAuth2Client(PUBLIC_GOOGLE_CLIENT_ID);
 
 // CORS headers da includere in ogni risposta
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': "ALLOWED_ORIGIN",
+  'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS'
 };
@@ -38,7 +38,7 @@ export async function POST({ request }) {
 
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: GOOGLE_CLIENT_ID
+      audience: PUBLIC_GOOGLE_CLIENT_ID
     });
 
     const payload = ticket.getPayload();
